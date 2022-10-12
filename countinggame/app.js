3b43b0015218c04657dc72f3fcb2bcd1c1c2eeec
RLM123456 <henningraffi50@gmail.com>
2022-10-12 11:44:56 +0100

    alor

--- countinggame/app.js
+++ countinggame/app.js
@@ -1,5 +1,6 @@
 let counterDisplayElem = document.querySelector('.counter-display');
 let counterMinusElem = document.querySelector('.counter-minus');
+let counterMinusEleme = document.querySelector('.counter-minusr');
 let counterPlusElem = document.querySelector('.counter-plus');
 
 let count = 0;
@@ -11,6 +12,10 @@ counterPlusElem.addEventListener("click",()=>{
 
 counterMinusElem.addEventListener("click",()=>{
     count--;
+    updateDisplay();
+});
+counterMinusEleme.addEventListener("click",()=>{
+    count-=count;
     updateDisplay();
 });
 
