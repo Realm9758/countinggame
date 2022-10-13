3e061e6be809bb4b17e2d3b7a4919ee06e983e72
RLM123456 <henningraffi50@gmail.com>
2022-10-13 11:49:22 +0100

    New button collums with random number guessers

--- countinggame/app.js
+++ countinggame/app.js
@@ -2,6 +2,9 @@ let counterDisplayElem = document.querySelector('.counter-display');
 let counterMinusElem = document.querySelector('.counter-minus');
 let counterMinusEleme = document.querySelector('.counter-minusr');
 let counterPlusElem = document.querySelector('.counter-plus');
+let counterPlusRand1 = document.querySelector('.counter-rand1');
+let counterPlusRand2 = document.querySelector('.counter-rand2');
+let counterPlusRand3 = document.querySelector('.counter-rand3');
 
 let count = 0;
 updateDisplay();
@@ -18,7 +21,21 @@ counterMinusEleme.addEventListener("click",()=>{
     count-=count;
     updateDisplay();
 });
-
+counterPlusRand1.addEventListener("click",()=>{
+    var y = Math.floor(Math.random() * 10 + 1);
+    count+=y;
+    updateDisplay();
+});
+counterPlusRand2.addEventListener("click",()=>{
+    var y = Math.floor(Math.random() * 100 + 1);
+    count+=y;
+    updateDisplay();
+});
+counterPlusRand3.addEventListener("click",()=>{
+    var y = Math.floor(Math.random() * 1000 + 1);
+    count+=y;
+    updateDisplay();
+});
 function updateDisplay(){
     counterDisplayElem.innerHTML = count;
 };
